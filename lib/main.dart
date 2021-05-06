9b9ff2f2fae5d74988fc280bf05a694a1ba7bd60
David Adegborioye <71905015+Beansnation@users.noreply.github.com>
2021-05-06 02:30:55 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,13 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+  //variables are declared above the build function
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  //a function that allows user to generate a radom number from 1 to 6
+  //the Random Class allows for number from 0, but there is no dice with a 0 number
+  //therefore 1 needs to be added to complement for that
   void changeDiceNumber(){
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
@@ -50,6 +54,8 @@ class _DicePageState extends State<DicePage> {
                   child: Image.asset('images/dice$leftDiceNumber.png'),),
               ),
             ),
+            //Expanded is being used because the dice image is bigger than the screen{512 X 512)
+            //the dice images take the available space provided by the screen and resizes itself
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
